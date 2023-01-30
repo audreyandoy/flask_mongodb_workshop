c571ad8682adabfc5389b9ece5d115907c87c5d1
Audrey Andoy <audreylandoy@gmail.com>
2023-01-30 12:49:31 -0500

    scaffolding for quick workshop

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,14 @@
 from flask import Flask
-from flask_pymongo import PyMongo
 import os
 from dotenv import load_dotenv
 
-mongo = PyMongo()
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-    
-    mongo.init_app(app)
-
     # Register Blueprints here
-    from app.routes.movies import movies_bp
-    app.register_blueprint(movies_bp)
+    # from app.routes.movies import movies_bp
+    # app.register_blueprint(movies_bp)
 
     return app
